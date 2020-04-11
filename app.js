e5e747679d209382a9bc1683a087f8446562f975
jmbodnar <jmbodnar@protonmail.com>
2020-04-11 15:08:40 -0400

    update users to visitors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var sassMiddleware = require("node-sass-middleware");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var visitorsRouter = require("./routes/visitors");
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/visitors", visitorsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
